9c77a60a1226341498d8e247dda26da4cf749b3c
Pyjacpp <pyjacpp@crans.org>
2026-09-19 10:31:28 +0200

    Remove unsigned for postresql
    Postgresql doen’t support the unsigned modifier cf https://www.postgresql.org/docs/current/datatype-numeric.html.
    
    Change-Id: I6e584ee333b95c35234954549a068e7111f7a826

--- sql/postgres/table_wsoauth_multiauth_mappings.sql
+++ sql/postgres/table_wsoauth_multiauth_mappings.sql
@@ -1,5 +1,5 @@
 CREATE TABLE /*_*/wsoauth_multiauth_mappings (
-	wsoauth_user int unsigned NOT NULL,
+	wsoauth_user int NOT NULL,
 	wsoauth_remote_name varchar(512) NOT NULL,
 	wsoauth_provider_id varchar(255) NOT NULL,
 	PRIMARY KEY (wsoauth_remote_name, wsoauth_provider_id)
